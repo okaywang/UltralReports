0f1c0c4daab578c8ebadae3818d2ea2f4fcf3b16
okaywang <44@qq.com>
2015-04-21 12:01:00 +0800

    generate data

---
+++ BussinessLogic/SE.BussinessLogic/UltraReportBussinessLogic.cs
@@ -0,0 +1,18 @@
+﻿using DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessLogic
+{
+    public class UltraReportBussinessLogic : BussinessLogicBase<UltraRecord>
+    {
+        public UltraReportBussinessLogic(EfRepository<UltraRecord> repository)
+            : base(repository)
+        {
+
+        }
+    }
+}
---
+++ Presentation/ConsoleApplicationUltraTest/Program.cs
@@ -0,0 +1,48 @@
+﻿using BussinessLogic;
+using DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplicationUltraTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenerateData_UltraRecord();
+
+            Console.WriteLine("abc");
+        }
+
+        private static void GenerateData_UltraRecord()
+        {
+            var context = new UltralReportsEntities();
+            var repository = new EfRepository<UltraRecord>(context);
+            var bll = new UltraReportBussinessLogic(repository);
+
+            var types = new[] { "L1", "L2", "L3", "H1", "H2", "H3" };
+            var symbols = new[] { 1, -1 };
+            var parts = new[] { 1, 2, 3 };
+            var duties = new[] { 1, 2, 3, 4, 5 };
+            var rand = new Random();
+            var records = new List<UltraRecord>();
+            for (int i = 0; i < 10000; i++)
+            {
+                var record = new UltraRecord();
+                record.UltraType = types[rand.Next(types.Length)];
+                record.PartId = parts[rand.Next(parts.Length)];
+                record.Duty = duties[rand.Next(duties.Length)];
+                record.BeginTime = DateTime.Now.AddMinutes(rand.Next(24 * 60) * symbols[rand.Next(2)]);
+                record.EndTime = record.BeginTime.AddMinutes(rand.Next(1, 20));
+                record.MinValue = rand.Next(500, 600);
+                record.MaxValue = record.MinValue + rand.Next(200);
+                record.AvgValue = (record.MinValue + record.MaxValue) / 2;
+                records.Add(record);
+            }
+            bll.InsertRange(records);
+        }
+    }
+}
