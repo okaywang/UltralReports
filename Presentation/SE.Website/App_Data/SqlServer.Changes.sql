1df6b60f6fa2a3ffdff67f7937b0f36bdb0fd8d3
okaywang <44@qq.com>
2015-05-22 18:34:25 +0800

    edit

--- Presentation/SE.Website/App_Data/SqlServer.Changes.sql
+++ Presentation/SE.Website/App_Data/SqlServer.Changes.sql
@@ -3,3 +3,16 @@ ALTER TABLE Part ALTER COLUMN L3 decimal(10,2) NULL
 ALTER TABLE Part ALTER COLUMN H2 decimal(10,2) NULL
 ALTER TABLE Part ALTER COLUMN H3 decimal(10,2) NULL
 ALTER TABLE Part ALTER COLUMN UltraNum int NULL
+
+ALTER TABLE UltraRecord Add  SmsFlag bit default(0) not null
+
+go
+create procedure SP_UltraTimes(@partId int,@times int output)
+as
+begin
+select @times = COUNT(*) from UltraRecord where PartId=@partId and StartTime>DATEADD(dd,-1,GETDATE())
+end
+--test
+declare @times int
+exec SP_UltraTimes 2,@times output
+select @times
