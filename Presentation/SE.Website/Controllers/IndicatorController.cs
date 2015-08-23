82f104cb71664aacf1cf0d9fea8ca881da48a251
myname <wo@qq.com>
2015-08-23 22:20:43 +0800

    edit

--- Presentation/SE.Website/Controllers/IndicatorController.cs
+++ Presentation/SE.Website/Controllers/IndicatorController.cs
@@ -2,7 +2,9 @@
 using DataAccess;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 using Website.Models;
@@ -29,6 +31,53 @@ namespace Website.Controllers
             //model.Weights = _bllKpi.GetLastestWeight(endDate.HasValue ? endDate.Value : DateTime.Now);
             model.Data = _bllKpi.Search(beginDate, endDate);
             return View(model);
+        }
+
+        public FileResult ExportExcel(DateTime? beginDate, DateTime? endDate)
+        {
+            var model = new KPIListPageModel();
+            //model.Weights = _bllKpi.GetLastestWeight(endDate.HasValue ? endDate.Value : DateTime.Now);
+            model.Data = _bllKpi.Search(beginDate, endDate);
+
+            this.ViewData.Model = model;
+            using (var sw = new StringWriter())
+            {
+                ViewEngineResult viewResult = System.Web.Mvc.ViewEngines.Engines.FindPartialView(this.ControllerContext, "_ExcelTemplate");
+                var viewContext = new ViewContext(this.ControllerContext, viewResult.View, this.ViewData, this.TempData, sw);
+                viewResult.View.Render(viewContext, sw);
+
+                var html = sw.GetStringBuilder().ToString();
+                byte[] fileContents = Encoding.UTF8.GetBytes(html);
+                return File(fileContents, "application/ms-excel", "指标竞赛.xls"); 
+            }
+        }
+
+        public FileResult ExportExcel1()
+        {
+            var sbHtml = new StringBuilder();
+            sbHtml.Append("<table border='1' cellspacing='0' cellpadding='0'>");
+            sbHtml.Append("<tr>");
+            var lstTitle = new List<string> { "编号", "姓名", "年龄", "创建时间" };
+            foreach (var item in lstTitle)
+            {
+                sbHtml.AppendFormat("<td style='font-size: 14px;text-align:center;background-color: #DCE0E2; font-weight:bold;' height='25'>{0}</td>", item);
+            }
+            sbHtml.Append("</tr>");
+
+            for (int i = 0; i < 1000; i++)
+            {
+                sbHtml.Append("<tr>");
+                sbHtml.AppendFormat("<td style='font-size: 12px;height:20px;'>{0}</td>", i);
+                sbHtml.AppendFormat("<td style='font-size: 12px;height:20px;'>屌丝{0}号</td>", i);
+                sbHtml.AppendFormat("<td style='font-size: 12px;height:20px;'>{0}</td>", new Random().Next(20, 30) + i);
+                sbHtml.AppendFormat("<td style='font-size: 12px;height:20px;'>{0}</td>", DateTime.Now);
+                sbHtml.Append("</tr>");
+            }
+            sbHtml.Append("</table>");
+
+            //第一种:使用FileContentResult
+            byte[] fileContents = Encoding.Default.GetBytes(sbHtml.ToString());
+            return File(fileContents, "application/ms-excel", "fileContents.xls"); 
         }
 
         [HttpPost]
