5f2fdab5e410a15c9df3170fc120b03f1c25754f
myname <wo@qq.com>
2015-09-09 22:04:41 +0800

    edit

--- Presentation/SE.Website/Controllers/HomeController.cs
+++ Presentation/SE.Website/Controllers/HomeController.cs
@@ -13,6 +13,7 @@ namespace Website.Controllers
 
         private CalcMonthReport instance = new ReportWS.CalcMonthReport();
 
+        [Authorize]
         public ActionResult Index()
         {
             return View();
@@ -23,12 +24,12 @@ namespace Website.Controllers
         {
             var model = new Table1Model();
             model.A1 = instance.GetValue("FYSIS.U1DCSAI.D1020");
-            model.B1 = instance.GetValue("FyFysis.CALC.A1003");
+            model.B1 = instance.GetValue("Fysis.CALC.A1003");
             model.C1 = instance.GetValue("Fysis.CALC.A1007");
             model.D1 = instance.GetValue("Fysis.CALC.A1012");
             model.E1 = instance.GetValue("Fysis.CALC.A1015");
             model.F1 = instance.GetValue("Fysis.CALC.A1016");
-            model.G1 = instance.GetValue("Fysis.TLOPCRTS.MCS02B:HBZS.RO01");
+            model.G1 = instance.GetValue("Fysis.TLOPCRTS.TLAIO535");
             model.H1 = instance.GetValue("Fysis.CALC.C0002");
 
             model.A2 = instance.GetValue("FYSIS.U2DCSAI.D1020");
@@ -37,7 +38,7 @@ namespace Website.Controllers
             model.D2 = instance.GetValue("Fysis.CALC.A2012");
             model.E2 = instance.GetValue("Fysis.CALC.A2015");
             model.F2 = instance.GetValue("Fysis.CALC.A2016");
-            model.G2 = instance.GetValue("Fysis.TLOPCRTS.MCS04B:HBZS.RO01");
+            model.G2 = instance.GetValue("Fysis.TLOPCRTS.TLAIO538");
 
             return PartialView("_Table1Partial", model);
         }
